08fbf5fbb0a27f278a8b000c9f02b1fa510ba4c0
Josh Greco <greco.joshua@gmail.com>
2019-08-21 21:58:29 -0400

    Done with first lecture

--- java-hello-world/src/Greeter.java
+++ java-hello-world/src/Greeter.java
@@ -2,28 +2,34 @@ public class Greeter {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        System.out.println("\nsout is shortcut");
+//        System.out.println("Hello World!");
+//        System.out.println("\nsout is shortcut");
+//
+//        greet("RIT");
+//        greet("CSH");
+//
+//
+//        if(true) { //hard coding T/F
+//            greet("RIT");
+//        }
+//        else {
+//            greet("CSH");
+//        }
+//
+//        boolean shouldGreetCSH = false;
+//
+//        if(shouldGreetCSH) {
+//            greet("RIT");
+//        }
+//        else {
+//            greet("CSH");
+//        }
+//
+//        greet("Josh");
 
-        greet("RIT");
-        greet("CSH");
-
-
-        if(true) { //hard coding T/F
-            greet("RIT");
-        }
-        else {
-            greet("CSH");
-        }
-
-        boolean shouldGreetCSH = false;
-
-        if(shouldGreetCSH) {
-            greet("RIT");
-        }
-        else {
-            greet("CSH");
-        }
+        System.out.println(args[0]);
+        //System.out.println(args[1]);
+        //System.out.println(args[2]);
     }
 
     static void greet(String name) {
